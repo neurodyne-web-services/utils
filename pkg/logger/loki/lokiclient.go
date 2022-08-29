10c3018cd5b3352b1f5a70b1dd75eeb3c648aa01
tetra12 <socmicro@yandex.com>
2022-08-29 18:31:20 +0300

    upd: loki client

--- pkg/logger/loki/lokiclient.go
+++ pkg/logger/loki/lokiclient.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/golang/snappy"
@@ -24,6 +25,11 @@ type serverResp struct {
 	body []byte
 }
 
+type streamItem struct {
+	labels string
+	entry  *v1.Entry
+}
+
 type lokiConfig struct {
 	url     string
 	ctype   string
@@ -40,36 +46,130 @@ func MakeLokiConfig(url, ctype, service string, level zapcore.Level) lokiConfig 
 	}
 }
 
+type batchConfig struct {
+	BatchEntriesNumber int
+	BatchWait          time.Duration
+}
+
 type lokiClient struct {
 	Client
+	enableLoki    bool
+	enableConsole bool
+	batch         batchConfig
 	conf          lokiConfig
 	http          http.Client
 	zl            *zap.Logger
-	enableLoki    bool
-	enableConsole bool
-	streams       chan []*v1.Stream
+	entries       chan streamItem
+	quit          chan struct{}
+	waitGroup     sync.WaitGroup
 }
 
 // MakeLokiLogger - factory for Loki client
-func MakeLokiLogger(conf lokiConfig, zl *zap.Logger, enaConsole, enaLoki bool) lokiClient {
-	return lokiClient{conf: conf, zl: zl, enableConsole: enaConsole, enableLoki: enaLoki}
+func MakeLokiLogger(conf lokiConfig, zl *zap.Logger, enaConsole, enaLoki bool) *lokiClient {
+	ch := make(chan streamItem, MAX_ENTRIES)
+
+	client := lokiClient{
+		enableConsole: enaConsole,
+		enableLoki:    enaLoki,
+		conf:          conf,
+		zl:            zl,
+		entries:       ch,
+	}
+	client.waitGroup.Add(1)
+	go client.run()
+
+	return &client
 }
 
-func (c lokiClient) Debugf(job, template string, args ...interface{}) {
+func (c *lokiClient) Debugf(job, template string, args ...interface{}) {
 
 	if c.enableConsole {
 		c.zl.Sugar().Debugf(template, args...)
 	}
 
 	if c.enableLoki && c.conf.level == zapcore.DebugLevel {
-		tmp := makeEntry(template, "Debug: ", args...)
-
 		labels := "{service=\"" + c.conf.service + "\",job=\"" + job + "\"}"
-		c.Process(labels, tmp)
+		c.push(labels, makeEntry(template, "Debug: ", args...))
 	}
 }
 
-func (c lokiClient) Push(buff *bytes.Buffer) (serverResp, error) {
+func (c *lokiClient) push(labels string, entry *v1.Entry) {
+	c.entries <- streamItem{
+		labels: labels,
+		entry:  entry,
+	}
+}
+
+func (c *lokiClient) run() {
+
+	var batch []*v1.Entry
+	batchSize := 0
+	maxWait := time.NewTimer(c.batch.BatchWait)
+
+	defer func() {
+		if batchSize > 0 {
+			c.process("my-labels", batch)
+		}
+
+		c.waitGroup.Done()
+	}()
+
+	for {
+		select {
+		case <-c.quit:
+			return
+		case entry := <-c.entries:
+			batch = append(batch, entry.entry)
+			batchSize++
+			if batchSize >= c.batch.BatchEntriesNumber {
+				c.process("my-labels", batch)
+				batch = []*v1.Entry{}
+				batchSize = 0
+				maxWait.Reset(c.batch.BatchWait)
+			}
+		case <-maxWait.C:
+			if batchSize > 0 {
+				c.process("my-labels", batch)
+				batch = []*v1.Entry{}
+				batchSize = 0
+			}
+			maxWait.Reset(c.batch.BatchWait)
+		}
+	}
+}
+
+func (c *lokiClient) process(labels string, entries []*v1.Entry) error {
+
+	var streams []*v1.Stream
+	streams = append(streams, &v1.Stream{
+		Labels:  labels,
+		Entries: entries,
+	})
+
+	req := v1.PushRequest{
+		Streams: streams,
+	}
+
+	pbuf, err := proto.Marshal(&req)
+	if err != nil {
+		return err
+	}
+
+	buf := snappy.Encode(nil, pbuf)
+
+	resp, err := c.send(bytes.NewBuffer(buf))
+	if err != nil {
+		return err
+	}
+
+	if resp.code != 204 {
+		return fmt.Errorf("invalid response code: %d", resp.code)
+	}
+
+	return nil
+}
+
+func (c *lokiClient) send(buff *bytes.Buffer) (serverResp, error) {
 	var out = serverResp{}
 
 	req, err := http.NewRequest("POST", c.conf.url, buff)
@@ -92,37 +192,6 @@ func (c lokiClient) Push(buff *bytes.Buffer) (serverResp, error) {
 	}
 
 	return out, nil
-}
-
-func (c lokiClient) Process(labels string, entry *v1.Entry) error {
-	var streams []*v1.Stream
-
-	streams = append(streams, &v1.Stream{
-		Labels:  labels,
-		Entries: []*v1.Entry{entry},
-	})
-
-	req := v1.PushRequest{
-		Streams: streams,
-	}
-
-	pbuf, err := proto.Marshal(&req)
-	if err != nil {
-		return err
-	}
-
-	buf := snappy.Encode(nil, pbuf)
-
-	resp, err := c.Push(bytes.NewBuffer(buf))
-	if err != nil {
-		return err
-	}
-
-	if resp.code != 204 {
-		return fmt.Errorf("invalid response code: %d", resp.code)
-	}
-
-	return nil
 }
 
 func makeEntry(format, prefix string, args ...interface{}) *v1.Entry {
