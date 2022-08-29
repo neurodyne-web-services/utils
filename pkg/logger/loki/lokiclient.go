1bfb6051d215f83544854ef6f063d3855097b54b
tetra12 <socmicro@yandex.com>
2022-08-29 16:07:26 +0300

    upd: initial impl of loki logger

--- pkg/logger/loki/lokiclient.go
+++ pkg/logger/loki/lokiclient.go
@@ -5,9 +5,14 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 
+	"github.com/golang/snappy"
+	v1 "github.com/neurodyne-web-services/utils/pkg/logger/loki/genout/v1"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type serverResp struct {
@@ -23,9 +28,10 @@ type lokiConfig struct {
 
 type lokiClient struct {
 	Client
-	conf lokiConfig
-	http http.Client
-	zl   *zap.Logger
+	conf  lokiConfig
+	http  http.Client
+	zl    *zap.Logger
+	items []v1.Stream
 }
 
 // MakeLokiClient - factory for Loki client
@@ -38,6 +44,13 @@ func (c lokiClient) Debugf(format string, args ...interface{}) {
 
 	if c.conf.level == zapcore.DebugLevel {
 		fmt.Println("Loki Debug called")
+		tmp := makeEntry(format, "Debug:", args...)
+
+		service_name := "loki-service"
+		job_name := "my-job-0"
+
+		labels := "{service=\"" + service_name + "\",job=\"" + job_name + "\"}"
+		c.Process(labels, tmp)
 	}
 }
 
@@ -64,4 +77,46 @@ func (c lokiClient) Push(buff *bytes.Buffer) (serverResp, error) {
 	}
 
 	return out, nil
+}
+
+func (c lokiClient) Process(labels string, entry *v1.Entry) error {
+	var streams []*v1.Stream
+
+	streams = append(streams, &v1.Stream{
+		Labels:  labels,
+		Entries: []*v1.Entry{entry},
+	})
+
+	req := v1.PushRequest{
+		Streams: streams,
+	}
+
+	pbuf, err := proto.Marshal(&req)
+	if err != nil {
+		return err
+	}
+
+	buf := snappy.Encode(nil, pbuf)
+
+	resp, err := c.Push(bytes.NewBuffer(buf))
+	if err != nil {
+		return err
+	}
+
+	if resp.code != 204 {
+		return fmt.Errorf("invalid response code: %d", resp.code)
+	}
+
+	return nil
+}
+
+func makeEntry(format, prefix string, args ...interface{}) *v1.Entry {
+	now := time.Now().UnixNano()
+	return &v1.Entry{
+		Timestamp: &timestamppb.Timestamp{
+			Seconds: now / int64(time.Second),
+			Nanos:   int32(now % int64(time.Second)),
+		},
+		Line: fmt.Sprintf(prefix+format, args...),
+	}
 }
