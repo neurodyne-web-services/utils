10f7527ad3105a7a0917e13cd78391ed5b386c5f
tetra12 <socmicro@yandex.com>
2022-08-31 08:52:04 +0300

    upd: logger test

--- pkg/logger/logger_test.go
+++ pkg/logger/logger_test.go
@@ -32,17 +32,25 @@ func Test_loki(t *testing.T) {
 	fmt.Println("Raw log:")
 	logger.Error("foo")
 	logger.Debug("My number is ", zap.Int("Number", 4))
+	logger.Warn("bar")
+	logger.Info("baz")
 }
+
+var items []string
 
 // A dummy logger, which implements a zap.WriteSyncer interface
 type dummyLogger struct{}
 
 func (l dummyLogger) Write(p []byte) (n int, err error) {
-	fmt.Printf(">>>> Loki logger: %s \n", string(p))
+	// fmt.Printf(">>>> Loki logger: %s \n", string(p))
+	if p != nil {
+		items = append(items, string(p))
+	}
+
 	return 0, nil
 }
 
 func (l dummyLogger) Sync() error {
-	fmt.Println("SYNC called")
+	fmt.Printf(">>>> SYNC: items size: %d", len(items))
 	return nil
 }
