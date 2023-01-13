ecfccb5928a586c8ee4aab90fc8a696206d3621a
tetra12 <socmicro@yandex.com>
2023-01-13 16:02:54 +0300

    feat: add json gzip utils

---
+++ pkg/json/json.go
@@ -0,0 +1,30 @@
+package json
+
+import (
+	"bytes"
+	"compress/gzip"
+	"encoding/json"
+)
+
+func MarshalGzip(data interface{}) ([]byte, error) {
+	buf := &bytes.Buffer{}
+	g := gzip.NewWriter(buf)
+	enc := json.NewEncoder(g)
+	if err := enc.Encode(data); err != nil {
+		return nil, err
+	}
+	if err := g.Flush(); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func UnmarshalGzip(b []byte, dst interface{}) error {
+	r := bytes.NewReader(b)
+	g, err := gzip.NewReader(r)
+	if err != nil {
+		return err
+	}
+	dec := json.NewDecoder(g)
+	return dec.Decode(dst)
+}
